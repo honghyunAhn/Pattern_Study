97700d4850a91f759d4c6968bccc0b89a6cc1691
honghyunAhn <anh2073@naver.com>
2022-01-15 23:47:54 +0900

    visitor

---
+++ src/visitor/ItemElement.java
@@ -0,0 +1,5 @@
+package visitor;
+
+public interface ItemElement {
+	int accept(ShoppingCartVisitor visitor);
+}
---
+++ src/visitor/ShoppingCartVisitorImpl.java
@@ -0,0 +1,27 @@
+package visitor;
+
+public class ShoppingCartVisitorImpl implements ShoppingCartVisitor {
+    //책 계산기
+    @Override
+    public int visit(Book book) {
+        int cost=0;
+        // book 값이 50달러 이상이면 5달러 할인
+        if(book.getPrice() > 50) {
+            cost = book.getPrice() - 5;
+        } else{
+            cost = book.getPrice();
+        }
+        System.out.println("책 번호 :: " + book.getIsbnNumber() + " 가격 = " + cost);
+
+        return cost;
+    }
+
+    //과일 계산기
+    @Override
+    public int visit(Fruit fruit) {
+        int cost = fruit.getPricePerKg()*fruit.getWeight();
+        System.out.println(fruit.getName() + " 가격 = "+cost);
+        return cost;
+    }
+}
+
